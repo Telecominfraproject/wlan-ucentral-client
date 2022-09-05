b0e7432966219f9c04a66299dcd7a026f0ff61a7
John Crispin <john@phrozen.org>
2022-09-05 09:40:12 +0200

    proto: limit wifiscans to 30s
    Signed-off-by: John Crispin <john@phrozen.org>

--- proto.c
+++ proto.c
@@ -573,7 +573,7 @@ configure_handle(struct blob_attr **rpc)
 }
 
 static void
-action_handle(struct blob_attr **rpc, char *command, int reply, int delay, int admin)
+action_handle(struct blob_attr **rpc, char *command, int reply, int delay, int admin, int timeout)
 {
 	struct blob_attr *tb[__PARAMS_MAX] = {};
 	uint32_t id = 0;
@@ -592,7 +592,7 @@ action_handle(struct blob_attr **rpc, char *command, int reply, int delay, int a
 	blob_buf_init(&action, 0);
 	blobmsg_add_string(&action, "command", command);
 	blobmsg_add_u32(&action, "delay", delay);
-	blobmsg_add_u32(&action, "timeout", 60 * 10);
+	blobmsg_add_u32(&action, "timeout", timeout ? timeout : 60 * 10);
 	if (rpc[JSONRPC_PARAMS]) {
 		void *c = blobmsg_open_table(&action, "payload");
 		struct blob_attr *b;
@@ -726,7 +726,7 @@ leds_handle(struct blob_attr **rpc)
 		blink_run(duration, id);
 		return;
 	}
-	action_handle(rpc, "leds", 1, 1, 1);
+	action_handle(rpc, "leds", 1, 1, 1, 0);
 }
 
 static void
@@ -919,13 +919,14 @@ proto_handle_blob(void)
 		else if (!strcmp(method, "reboot") ||
 			 !strcmp(method, "factory") ||
 			 !strcmp(method, "upgrade"))
-			action_handle(rpc, method, 1, 10, 1);
+			action_handle(rpc, method, 1, 10, 1, 0);
 		else if (!strcmp(method, "perform") ||
 			 !strcmp(method, "rtty") ||
-			 !strcmp(method, "wifiscan") ||
 			 !strcmp(method, "script") ||
 			 !strcmp(method, "trace"))
-			action_handle(rpc, method, 0, 1, 0);
+			action_handle(rpc, method, 0, 1, 0, 0);
+		else if (!strcmp(method, "wifiscan"))
+			action_handle(rpc, method, 0, 1, 0, 30);
 		else if (!strcmp(method, "leds"))
 			leds_handle(rpc);
 		else if (!strcmp(method, "request"))
