a94e2dfd5ce11ef96876040713e5f5f72555797c
Satya-sc <Satya@shasta.cloud>
2023-04-26 20:39:19 +0530

    ucentral-client: added the support to remove old public IP file once it is connected successfully to controller
    Signed-off-by: Satya-sc <Satya@shasta.cloud>

--- main.c
+++ main.c
@@ -10,6 +10,8 @@
 
 #include "ucentral.h"
 
+#define PUBLIC_IP_FILE "/tmp/public_ip"
+
 static int reconnect_timeout;
 static int bad_CN;
 static struct lws_context *context;
@@ -236,6 +238,7 @@ callback_broker(struct lws *wsi, enum lws_callback_reasons reason,
 		reconnect_timeout = 1;
 		set_conn_time();
 		websocket = wsi;
+		remove(PUBLIC_IP_FILE);
 		connect_send();
 		crashlog_init();
 		event_backlog();
